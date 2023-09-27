7bd8d8d758de7030d96bfef7209889ffbf35b71a
brianriwukaho <b.riwukaho@gmail.com>
2023-09-27 17:59:54 +1000

    feat: group d3 helpers into own helpers file

--- src/App.svelte
+++ src/App.svelte
@@ -2,103 +2,19 @@
   import { onMount } from "svelte";
   import * as d3 from "d3";
   import { data } from "./stub";
-
-  interface NodeData {
-    name: string;
-    children?: NodeData[];
-  }
-
-  type PositionedNode = d3.HierarchyNode<NodeData> & {
-    x: number;
-    y: number;
-  };
-
-  interface PositionedLink extends d3.HierarchyLink<NodeData> {
-    source: PositionedNode;
-    target: PositionedNode;
-  }
+  import {
+    type PositionedNode,
+    type PositionedLink,
+    initZoom,
+    drawLines,
+    drawCircles,
+    drawText,
+  } from "./lib/d3.helpers";
 
   const width = 600;
   const height = 400;
   const dx = width / 2;
   const dy = -200;
-
-  function initZoom(
-    svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
-    svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>
-  ) {
-    const zoom = d3
-      .zoom()
-      .scaleExtent([0.1, 5])
-      .on("zoom", (event) => {
-        svgGroup.attr("transform", event.transform.toString());
-      });
-
-    // @ts-ignore
-    svg.call(zoom);
-
-    const initialScale = 0.2;
-    const initialX = (width * (1 - initialScale)) / 2;
-    const initialY = (height * (1 - initialScale)) / 2;
-
-    svg.call(
-      // @ts-ignore
-      zoom.transform,
-      d3.zoomIdentity.translate(initialX, initialY).scale(initialScale)
-    );
-  }
-
-  function drawLines(
-    svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
-    links: PositionedLink[]
-  ) {
-    svgGroup
-      .selectAll("line")
-      .data(links)
-      .enter()
-      .append("line")
-      .attr("x1", (d) => d.source.x + dx)
-      .attr("y1", (d) => d.source.y + dy)
-      .attr("x2", (d) => d.target.x + dx)
-      .attr("y2", (d) => d.target.y + dy)
-      .attr("stroke", "black")
-      .attr("stroke-width", 1.5);
-  }
-
-  function drawCircles(
-    svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
-    rootNode: PositionedNode
-  ) {
-    svgGroup
-      .selectAll("circle")
-      .data(rootNode.descendants())
-      .enter()
-      .append("circle")
-      .attr("cx", (d: PositionedNode) => d.x + dx)
-      .attr("cy", (d: PositionedNode) => d.y + dy)
-      .attr("r", 40)
-      .style("fill", "#eee")
-      .style("stroke", "black")
-      .style("stroke-width", "1.5")
-      .on("click", (event, d) => console.log("click", d));
-  }
-
-  function drawText(
-    svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
-    rootNode: PositionedNode
-  ) {
-    svgGroup
-      .selectAll("text")
-      .data(rootNode.descendants())
-      .enter()
-      .append("text")
-      .attr("x", (d: PositionedNode) => d.x + dx)
-      .attr("y", (d: PositionedNode) => d.y + dy)
-      .attr("dy", "0.32em")
-      .attr("text-anchor", "middle")
-      .attr("font-size", "3.25em")
-      .text((d) => d.data.name);
-  }
 
   onMount(() => {
     const svg = d3
@@ -112,10 +28,10 @@
     const treeLayout = d3.tree().size([height, width]).nodeSize([125, 250]);
     const links: PositionedLink[] = treeLayout(rootNode as any).links() as any;
 
-    initZoom(svg, svgGroup);
-    drawLines(svgGroup, links);
-    drawCircles(svgGroup, rootNode);
-    drawText(svgGroup, rootNode);
+    initZoom(svg, svgGroup, height, width);
+    drawLines(svgGroup, links, dx, dy);
+    drawCircles(svgGroup, rootNode, dx, dy);
+    drawText(svgGroup, rootNode, dx, dy);
   });
 </script>
 
---
+++ src/lib/d3.helpers.ts
@@ -0,0 +1,101 @@
+import * as d3 from "d3";
+
+export interface NodeData {
+  name: string;
+  children?: NodeData[];
+}
+
+export type PositionedNode = d3.HierarchyNode<NodeData> & {
+  x: number;
+  y: number;
+};
+
+export interface PositionedLink extends d3.HierarchyLink<NodeData> {
+  source: PositionedNode;
+  target: PositionedNode;
+}
+
+export function initZoom(
+  svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
+  svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
+  height: number,
+  width: number
+) {
+  const zoom = d3
+    .zoom()
+    .scaleExtent([0.1, 5])
+    .on("zoom", (event) => {
+      svgGroup.attr("transform", event.transform.toString());
+    });
+
+  // @ts-ignore
+  svg.call(zoom);
+
+  const initialScale = 0.2;
+  const initialX = (width * (1 - initialScale)) / 2;
+  const initialY = (height * (1 - initialScale)) / 2;
+
+  svg.call(
+    // @ts-ignore
+    zoom.transform,
+    d3.zoomIdentity.translate(initialX, initialY).scale(initialScale)
+  );
+}
+
+export function drawLines(
+  svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
+  links: PositionedLink[],
+  x: number,
+  y: number
+) {
+  svgGroup
+    .selectAll("line")
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("x1", (d) => d.source.x + x)
+    .attr("y1", (d) => d.source.y + y)
+    .attr("x2", (d) => d.target.x + x)
+    .attr("y2", (d) => d.target.y + y)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1.5);
+}
+
+export function drawCircles(
+  svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
+  rootNode: PositionedNode,
+  x: number,
+  y: number
+) {
+  svgGroup
+    .selectAll("circle")
+    .data(rootNode.descendants())
+    .enter()
+    .append("circle")
+    .attr("cx", (d: PositionedNode) => d.x + x)
+    .attr("cy", (d: PositionedNode) => d.y + y)
+    .attr("r", 40)
+    .style("fill", "#eee")
+    .style("stroke", "black")
+    .style("stroke-width", "1.5")
+    .on("click", (event, d) => console.log("click", d));
+}
+
+export function drawText(
+  svgGroup: d3.Selection<SVGGElement, unknown, HTMLElement, any>,
+  rootNode: PositionedNode,
+  x: number,
+  y: number
+) {
+  svgGroup
+    .selectAll("text")
+    .data(rootNode.descendants())
+    .enter()
+    .append("text")
+    .attr("x", (d: PositionedNode) => d.x + x)
+    .attr("y", (d: PositionedNode) => d.y + y)
+    .attr("dy", "0.32em")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "3.25em")
+    .text((d) => d.data.name);
+}
